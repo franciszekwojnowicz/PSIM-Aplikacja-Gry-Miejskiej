b410f62e5e23569e3b9f81bfabb2c0feaafb9ec7
Franciszek Wojnowicz <62144431+franciszekwojnowicz@users.noreply.github.com>
2024-03-29 16:35:33 +0100

    structure and models fix

--- Backend/urban_game/main_app/admin.py
+++ Backend/urban_game/main_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(User)
+admin.site.register(Comment)
+admin.site.register(Achivement)
+admin.site.register(Unlocked_Achivement)
+admin.site.register(Visited_Restaurant)
+admin.site.register(Rating)
--- Backend/urban_game/main_app/views.py
+++ Backend/urban_game/main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .serializers import *
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
+
+#simple views connection test
+def test(request):
+    if request.method == 'GET':
+        return HttpResponse(200)
+    else:
+        return HttpResponse(400)
+        
