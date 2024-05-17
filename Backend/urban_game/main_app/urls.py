dd922c92ad1bbcaec29f5dae2c64f3f81f9bdebb
Franciszek Wojnowicz <62144431+franciszekwojnowicz@users.noreply.github.com>
2024-05-17 17:26:31 +0200

    some endpoints chnges

--- Backend/urban_game/main_app/urls.py
+++ Backend/urban_game/main_app/urls.py
@@ -3,15 +3,25 @@ from . import views
 
 urlpatterns = [
     path('test', views.test),
-    path('viewAllRestaurants', views.viewAllRestaurants),
-    path('viewAllAchivements', views.viewAllAchivements),
-    path('viewAllUsers', views.viewAllUsers),
-    path('viewUserAchivements', views.viewUserAchivements),
-    path('viewUserVisited_Restaurants', views.viewUserVisited_Restaurants),
-    path('addVisited_Restaurant', views.addVisited_Restaurant),
+    #path('viewAllAchivements', views.viewAllAchivements),
+    #path('viewUserAchivements', views.viewUserAchivements),
+    path('achivements', views.viewUserAchivements),
+
+    #path('viewAllRestaurants', views.viewAllRestaurants),
+    #path('viewUserVisited_Restaurants', views.viewUserVisited_Restaurants),
+    #path('addVisited_Restaurant', views.addVisited_Restaurant),
+    path('restaurants', views.restaurants),
+
+    
+
+    path('users', views.viewAllUsers),
+    
+    
+    
     path('addComment', views.addComment),
     path('viewCommentsForRestaurant',views.viewCommentsForRestaurant),
     path('register/', views.register_user), 
     path('login/', views.login_user),
+
     path('addDefaultAchivements', views.addDefaultAchivements), 
 ]
--- Backend/urban_game/main_app/views.py
+++ Backend/urban_game/main_app/views.py
@@ -48,7 +48,7 @@ def login_user(request):
         return Response({"token": token.key})
     return Response(status=400, data="Invalid credentials")
     
-@api_view(['GET'])
+@api_view(['GET']) ## UNUSED replaced by viewUserVisited_Restaurants()
 def viewAllRestaurants(request):
     try:
         restaurants_database=Restaurant.objects.all()
@@ -57,7 +57,7 @@ def viewAllRestaurants(request):
     except Exception as e:
         return Response(status=400, data=repr(e))
 
-@api_view(['GET'])
+@api_view(['GET'])  ## UNUSED replaced by viewUserAchivements()
 def viewAllAchivements(request):
     try:
         achivements_database=Achivement.objects.all()
@@ -87,13 +87,111 @@ def viewUserAchivements(request):
         for unlocked_achivement in unlocked_achivements:
             achivement_id.append(unlocked_achivement["achivement"])
         # filtrujemy po id z listy
-        achivements=Achivement.objects.filter(id__in=achivement_id)
-        achivement_serialized=AchivementSerializer(achivements,many=True)
-        return JsonResponse(achivement_serialized.data,safe=False)
+        achivements_unlocked=Achivement.objects.filter(id__in=achivement_id)
+        achivements_locked=Achivement.objects.all().exclude(id__in=achivement_id)
+        achivements_unlocked_serialized=AchivementSerializer(achivements_unlocked,many=True)
+        achivements_locked_serialized=AchivementSerializer(achivements_locked,many=True)
+        achivements_serialized={}
+        achivements_serialized["unlocked"]=achivements_unlocked_serialized.data
+        achivements_serialized["locked"]=achivements_locked_serialized.data
+        return JsonResponse(achivements_serialized,safe=False)
     except Exception as e:
         return Response(status=400, data=repr(e))
-    
-@api_view(['GET'])
+
+@api_view(['GET','POST'])
+def restaurants(request):
+    if request.method == "GET":
+        try:
+            id = request.GET.get('user')
+            visited_restaurant_database=Visited_Restaurant.objects.filter(user=id)
+            visited_restaurant_serialized=Visited_RestaurantSerializer(visited_restaurant_database,many=True)
+            visited_restaurants=visited_restaurant_serialized.data
+            restaurant_id=[]
+            # tworzymy listę id odwiedzonych resauracji
+            for visited_restaurant in visited_restaurants:
+                restaurant_id.append(visited_restaurant["restaurant"])
+            # filtrujemy po id z listy
+            restaurants_unlocked=Restaurant.objects.filter(id__in=restaurant_id)
+            restaurants_locked=Restaurant.objects.all().exclude(id__in=restaurant_id)
+            restaurants_unlocked_serialized=RestaurantSerializer(restaurants_unlocked,many=True)
+            restaurants_locked_serialized=RestaurantSerializer(restaurants_locked,many=True)
+            restaurant_serialized={}
+            restaurant_serialized["unlocked"]=restaurants_unlocked_serialized.data
+            restaurant_serialized["locked"]=restaurants_locked_serialized.data
+            return JsonResponse(restaurant_serialized,safe=False)
+        except Exception as e:
+            return Response(status=400, data=repr(e))
+    elif request.method == "POST":
+        try:
+            code = request.data.get('code')
+            user = request.data.get('user')
+            restaurant_database=Restaurant.objects.get(unlock_code=code)
+            user_database=User.objects.get(id=user)
+            visited_restaurant=Visited_Restaurant(user=user_database,restaurant=restaurant_database)
+            visited_restaurant.full_clean()
+            visited_restaurant.save()
+            #tutaj można wstawić jakieś warunki do achivementów, np sprawdza ile restauracji danego typu odwiedzono i dodaje inlocked_achivement do usera
+            visited_restaurant_database = Visited_Restaurant.objects.filter(user=user)
+            visited_types_serialized = Visited_Restaurants_TypeSerializer(visited_restaurant_database,many=True)
+            visited_types = visited_types_serialized.data
+            sushi_count = 0
+            pizza_count = 0
+            kebab_count = 0
+            for visited_type in visited_types:
+                if visited_type["type"]=="sushi":
+                    sushi_count+=1
+                if visited_type["type"]=="pizza":
+                    pizza_count+=1
+                if visited_type["type"]=="kebab":
+                    kebab_count+=1
+
+            user_database=User.objects.get(id=user)
+            user_database.points += 1
+            if sushi_count>1:
+                achivement_database=Achivement.objects.get(name="sushi beginer")
+                new_achivement=Unlocked_Achivement(user=user_database,achivement=achivement_database)
+                if Unlocked_Achivement.objects.filter(user=user_database,achivement=achivement_database).exists() == False:
+                    new_achivement.save()
+                    user_database.points += 10
+            if sushi_count>5:
+                achivement_database=Achivement.objects.get(name="sushi master")
+                new_achivement=Unlocked_Achivement(user=user_database,achivement=achivement_database)
+                if Unlocked_Achivement.objects.filter(user=user_database,achivement=achivement_database).exists() == False:
+                    new_achivement.save()
+                    user_database.points += 30
+
+            if pizza_count>1:
+                achivement_database=Achivement.objects.get(name="pizza beginer")
+                new_achivement=Unlocked_Achivement(user=user_database,achivement=achivement_database)
+                if Unlocked_Achivement.objects.filter(user=user_database,achivement=achivement_database).exists() == False:
+                    new_achivement.save()
+                    user_database.points += 10
+            if pizza_count>5:
+                achivement_database=Achivement.objects.get(name="pizza master")
+                new_achivement=Unlocked_Achivement(user=user_database,achivement=achivement_database)
+                if Unlocked_Achivement.objects.filter(user=user_database,achivement=achivement_database).exists() == False:
+                    new_achivement.save()
+                    user_database.points += 30
+
+            if kebab_count>1:
+                achivement_database=Achivement.objects.get(name="kebab beginer")
+                new_achivement=Unlocked_Achivement(user=user_database,achivement=achivement_database)
+                if Unlocked_Achivement.objects.filter(user=user_database,achivement=achivement_database).exists() == False:
+                    new_achivement.save()
+                    user_database.points += 10
+            if kebab_count>5:
+                achivement_database=Achivement.objects.get(name="kebab master")
+                new_achivement=Unlocked_Achivement(user=user_database,achivement=achivement_database)
+                if Unlocked_Achivement.objects.filter(user=user_database,achivement=achivement_database).exists() == False:
+                    new_achivement.save()
+                    user_database.points += 30
+            user_database.save()
+            return Response(visited_types_serialized.data,status=200)
+        except Exception as e:
+            return Response(status=400, data=repr(e))
+        
+
+@api_view(['GET']) ## UNUSED
 def viewUserVisited_Restaurants(request):
     try:
         id = request.GET.get('user')
@@ -105,9 +203,14 @@ def viewUserVisited_Restaurants(request):
         for visited_restaurant in visited_restaurants:
             restaurant_id.append(visited_restaurant["restaurant"])
         # filtrujemy po id z listy
-        restaurants=Restaurant.objects.filter(id__in=restaurant_id)
-        restaurant_serialized=RestaurantSerializer(restaurants,many=True)
-        return JsonResponse(restaurant_serialized.data,safe=False)
+        restaurants_unlocked=Restaurant.objects.filter(id__in=restaurant_id)
+        restaurants_locked=Restaurant.objects.all().exclude(id__in=restaurant_id)
+        restaurants_unlocked_serialized=RestaurantSerializer(restaurants_unlocked,many=True)
+        restaurants_locked_serialized=RestaurantSerializer(restaurants_locked,many=True)
+        restaurant_serialized={}
+        restaurant_serialized["unlocked"]=restaurants_unlocked_serialized.data
+        restaurant_serialized["locked"]=restaurants_locked_serialized.data
+        return JsonResponse(restaurant_serialized,safe=False)
     except Exception as e:
         return Response(status=400, data=repr(e))
 
@@ -127,11 +230,11 @@ def viewCommentsForRestaurant(request):
     except Exception as e:
         return Response(status=400, data=repr(e))   
 
-@api_view(['GET'])  
+@api_view(['POST'])  
 def addVisited_Restaurant(request):
     try:
-        code = request.GET.get('code')
-        user = request.GET.get('user')
+        code = request.data.get('code')
+        user = request.data.get('user')
         restaurant_database=Restaurant.objects.get(unlock_code=code)
         user_database=User.objects.get(id=user)
         visited_restaurant=Visited_Restaurant(user=user_database,restaurant=restaurant_database)
