b80cb0e392fe57cd9b8a64ef1aa7885a56b74461
kvpe <jakub.kolbertowicz@gmail.com>
2024-06-05 00:35:06 +0200

    changed staticfiles location

--- Backend/urban_game/urban_game/urls.py
+++ Backend/urban_game/urban_game/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    
     path('admin/', admin.site.urls),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
     path('', include('main_app.urls')),
 ]
