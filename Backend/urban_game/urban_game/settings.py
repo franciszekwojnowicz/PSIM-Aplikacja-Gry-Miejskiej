652b848d56d803efefdbe4a1282acc8cb03a8054
kvpe <jakub.kolbertowicz@gmail.com>
2024-06-05 00:59:30 +0200

    changed cors allowed origins

--- Backend/urban_game/urban_game/settings.py
+++ Backend/urban_game/urban_game/settings.py
@@ -165,7 +165,12 @@ REST_FRAMEWORK = {
     ),
 }
 
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'https://psim-aplikacja-gry-miejskiej.onrender.com',
 ]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    CORS_ALLOWED_ORIGINS.append(f'https://{RENDER_EXTERNAL_HOSTNAME}')
