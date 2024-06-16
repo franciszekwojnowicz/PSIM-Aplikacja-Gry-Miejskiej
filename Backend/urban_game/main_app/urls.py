507b2b68de484d221776203cc55cb6f9c4039b5b
kvpe <jakub.kolbertowicz@gmail.com>
2024-06-16 18:21:23 +0200

    changed urls.py

--- Backend/urban_game/main_app/urls.py
+++ Backend/urban_game/main_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    #re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
     path('api/user/', views.viewAllUsers),   
     path('api/user/<int:user_id>/', views.viewUser),
     path('api/user/<int:user_id>/restaurants/', views.viewUserRestaurants),
@@ -17,4 +17,6 @@ urlpatterns = [
     path('api/login/', views.login_user),
 
     path('api/addDefaultAchievements', views.addDefaultAchivements),
+    
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
